39e4e2ca0962fd628d335b1aa7a3d9c20d11a30b
Tian <tianmaodi@gmail.com>
2024-11-21 14:32:37 +0800

    update: https=>http.

--- src/module/fundMNHisNetList.js
+++ src/module/fundMNHisNetList.js
@@ -4,6 +4,7 @@ const { post } = require('../utils/index.js');
  * 获取基金历史净值
  */
 module.exports = async (params = {}) => {
-  const url = 'https://fundmobapi.eastmoney.com/FundMNewApi/FundMNHisNetList';
+  // const url = 'https://fundmobapi.eastmoney.com/FundMNewApi/FundMNHisNetList';
+  const url = 'http://fundmobapi.eastmoney.com/FundMNewApi/FundMNHisNetList';
   return post(url, params);
 };
